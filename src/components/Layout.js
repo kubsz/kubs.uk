84e69d3a22b01fb2b7adbc5020715a4cc49fb2fd
Kubs <me@kubs.uk>
2021-08-13 18:47:41 +0100

    added default title

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,9 +1,13 @@
+import Head from 'next/head';
 import Footer from './Footer';
 import Nav from './Nav';
 
-const Layout = ({ harshFooterShadow, children }) => {
+const Layout = ({ harshFooterShadow, children, title }) => {
     return (
         <div className="root">
+            <Head>
+                <title>{title || 'Kubs K - Web Developer'}</title>
+            </Head>
             <div className="root__overlay" id="root__overlay"></div>
             <Nav />
             <main className="main">{children}</main>
