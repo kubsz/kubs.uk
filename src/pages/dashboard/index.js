771bf37f562981a72f2e5e3e4011f8344667a644
Kubs <me@kubs.uk>
2021-08-16 22:17:11 +0100

    added health api implementation

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,14 +1,44 @@
+import { createClient } from '@supabase/supabase-js';
 import Layout from '../../components/Layout';
 import Section from '../../components/Section';
 
-const DashboardIndex = () => {
+const DashboardIndex = ({ health }) => {
+    console.log(health[0]);
     return (
         <Layout>
             <Section>
-                <div className="pg-dashboard">dashboard index</div>
+                {health.map((obj) => (
+                    <>
+                        <h3 key={obj.name}>{obj.name}:</h3>
+                        {obj.data.map((x, i) => (
+                            <p key={i}>
+                                <strong>{x.date}: </strong>
+                                {x.qty}
+                            </p>
+                        ))}
+                    </>
+                ))}
             </Section>
         </Layout>
     );
 };
 
 export default DashboardIndex;
+
+/* eslint-disable */
+
+export const getStaticProps = async () => {
+    const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+    const supabaseKey = process.env.SUPABASE_SECRET_KEY;
+
+    const supabase = createClient(supabaseUrl, supabaseKey);
+
+    let { data: health, error } = await supabase.from('health').select('data');
+
+    return {
+        props: {
+            health: health[0].data
+        },
+        revalidate: 10
+    };
+};
