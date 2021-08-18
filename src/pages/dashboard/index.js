d5963d84cc6d3555568cca5c955c5d2c947c4208
Kubs <me@kubs.uk>
2021-08-18 15:58:53 +0100

    began using another chart package

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,13 +1,11 @@
 /* eslint-disable */
 
-import React from 'react';
+import React, { useState } from 'react';
 
-import { LineChart } from '@carbon/charts-react';
-
-import '@carbon/charts/styles.css';
+import { Line } from 'react-chartjs-2';
 
 import { createClient } from '@supabase/supabase-js';
-import { formatDistance, format } from 'date-fns';
+import { formatDistance, format, isBefore } from 'date-fns';
 import useSWR from 'swr';
 import Layout from '../../components/Layout';
 import Section from '../../components/Section';
@@ -23,65 +21,63 @@ const fetcher = (query) => {
 const DashboardIndex = () => {
     const { data, error } = useSWR(process.env.NEXT_PUBLIC_SUPABASE_URL, fetcher);
 
+    const [period, setPeriod] = useState(30);
+
     if (!data) return <p>loading...</p>;
 
     const weight = data.data.find((x) => x.ref === 'weight_body_mass');
     const entries = weight.entry;
 
-    const maxValue = entries.reduce((acc, shot) => (acc = acc > shot.data ? acc : shot.data), 0) * 1;
-    const minValue = entries.reduce((acc, shot) => (acc = acc < shot.data ? acc : shot.data)) * 1;
-    const diff = maxValue - minValue;
-    const intervals = 5;
+    const periodStart = new Date();
+    periodStart.setDate(periodStart.getDate() - period);
 
-    const step = diff / intervals;
+    const entriesInPeriod = period ? entries.filter((entry) => isBefore(periodStart, new Date(entry.date))) : entries;
 
-    console.log(step);
+    const chartData = {
+        labels: entriesInPeriod.map((x) => format(new Date(x.date), 'dd LLLL, yyyy')),
+        datasets: [
+            {
+                label: 'Weight',
+                data: entriesInPeriod.map((x) => x.data.toFixed(2)),
+                fill: false,
+                backgroundColor: 'rgb(255, 99, 132)',
+                borderColor: 'rgba(255, 99, 132, 0.2)'
+            }
+        ]
+    };
 
-    const axis = [
-        minValue - step,
-        minValue,
-        ...Array.from(Array(intervals).keys()).map((x, i) => minValue + step * (i + 1)),
-        maxValue,
-        maxValue + step
-    ].map((x) => x.toFixed(2));
+    const options = {
+        scales: {
+            yAxes: [
+                {
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }
+            ]
+        }
+    };
 
     return (
         <Layout>
             <Section>
                 <h1>My Bodyweight</h1>
-                <LineChart
-                    data={[...entries]
-                        .reverse()
-                        .splice(1, 30)
-                        .map((entry) => ({
-                            group: 'xxxx',
-                            date: format(new Date(entry.date), 'MM-dd-yyyy'),
-                            value: entry.data
-                        }))}
-                    options={{
-                        resizable: true,
-                        zoomBar: true,
-                        title: 'Bodyweight',
-                        axes: {
-                            left: {
-                                title: 'Weight (kg)',
-                                mapsTo: 'value',
-                                scaleType: 'linear',
-                                includeZero: false,
-                                ticks: {
-                                    values: axis
-                                }
-                            },
-                            bottom: {
-                                title: 'Date',
-                                mapsTo: 'date',
-                                scaleType: 'labels'
-                            }
-                        },
-                        curve: 'curveMonotoneX',
-                        height: '400px'
-                    }}
-                />
+                <button onClick={() => setPeriod(7)} style={{ backgroundColor: period === 7 ? 'red' : 'green' }}>
+                    7 days
+                </button>
+                <button onClick={() => setPeriod(30)} style={{ backgroundColor: period === 30 ? 'red' : 'green' }}>
+                    30 days
+                </button>
+                <button onClick={() => setPeriod(90)} style={{ backgroundColor: period === 90 ? 'red' : 'green' }}>
+                    3 months
+                </button>
+                <button onClick={() => setPeriod(365)} style={{ backgroundColor: period === 365 ? 'red' : 'green' }}>
+                    1 year
+                </button>
+                <button onClick={() => setPeriod(null)} style={{ backgroundColor: period === null ? 'red' : 'green' }}>
+                    All Time
+                </button>
+                <Line data={chartData} options={options} />
                 <div className="pg-dashboard">
                     {[...entries].reverse().map((entry, i) => (
                         <div key={i} className="pg-dashboard__row">
